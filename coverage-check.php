aeb8f51141974fd4e617b10956d401a02639e16a
Niko Heikkilä <niko.heikkila@paytrail.com>
2019-10-13 18:27:37 +0300

    Refactor and optimize code
    - Guard against empty data (division by zero)
    - Refactor logic to functions and constants
    - Make 'only-percentage' option more canonical by prepending with two dashes
    - Update tests

--- coverage-check.php
+++ coverage-check.php
@@ -1,26 +1,46 @@
 <?php
+
+declare(strict_types=1);
+
 // Inspired by: https://ocramius.github.io/blog/automated-code-coverage-check-for-github-pull-requests-with-travis/
-if (!isset($argv[1]) || !file_exists($argv[1])) {
-    echo 'Invalid input file provided' . PHP_EOL;
-    exit(1);
+
+const XPATH_METRICS = '//metrics';
+const STATUS_OK = 0;
+const STATUS_ERROR = 1;
+
+function formatCoverage(float $number): string
+{
+    return sprintf('%0.2f %%', $number);
 }
 
-if (!isset($argv[2])) {
-    echo 'An integer checked percentage must be given as second parameter'. PHP_EOL;
-    exit(1);
+function loadMetrics(string $file): array
+{
+    $xml = new SimpleXMLElement(file_get_contents($file));
+
+    return $xml->xpath(XPATH_METRICS);
 }
 
-$onlyEchoPercentage = false;
-
-if (isset($argv[3]) && $argv[3] === 'only-percentage') {
-    $onlyEchoPercentage = true;
+function printStatus(string $msg, int $exitCode = STATUS_OK)
+{
+    echo $msg.PHP_EOL;
+    exit($exitCode);
 }
+
+if (! isset($argv[1]) || ! file_exists($argv[1])) {
+    printStatus("Invalid input file {$argv[1]} provided.", STATUS_ERROR);
+}
+
+if (! isset($argv[2])) {
+    printStatus(
+        'An integer checked percentage must be given as second parameter.',
+        STATUS_ERROR
+    );
+}
+
+$onlyEchoPercentage = isset($argv[3]) && $argv[3] === '--only-percentage';
 
 $inputFile = $argv[1];
-$percentage = min(100, max(0, (float)$argv[2]));
-
-$xml = new SimpleXMLElement(file_get_contents($inputFile));
-$metrics = $xml->xpath('//metrics');
+$percentage = min(100, max(0, (float) $argv[2]));
 
 $elements = 0;
 $coveredElements = 0;
@@ -29,31 +49,38 @@ $coveredstatements = 0;
 $methods = 0;
 $coveredmethods = 0;
 
-foreach ($metrics as $metric) {
-    $elements += (int)$metric['elements'];
-    $coveredElements += (int)$metric['coveredelements'];
-    $statements += (int)$metric['statements'];
-    $coveredstatements += (int)$metric['coveredstatements'];
-    $methods += (int)$metric['methods'];
-    $coveredmethods += (int)$metric['coveredmethods'];
+foreach (loadMetrics($inputFile) as $metric) {
+    $elements += (int) $metric['elements'];
+    $coveredElements += (int) $metric['coveredelements'];
+    $statements += (int) $metric['statements'];
+    $coveredstatements += (int) $metric['coveredstatements'];
+    $methods += (int) $metric['methods'];
+    $coveredmethods += (int) $metric['coveredmethods'];
 }
 
 // See calculation: https://confluence.atlassian.com/pages/viewpage.action?pageId=79986990
-$TPC = ($coveredstatements + $coveredmethods + $coveredElements) / ($statements + $methods + $elements) * 100;
+$coveredMetrics = $coveredstatements + $coveredmethods + $coveredElements;
+$totalMetrics = $statements + $methods + $elements;
 
-if ($TPC < $percentage && ! $onlyEchoPercentage) {
-    echo 'Total code coverage is ' . sprintf('%0.2f', $TPC) . '%, which is below the accepted ' . $percentage . '%' . PHP_EOL;
-    exit(1);
+if ($totalMetrics === 0) {
+    printStatus('Insufficient data for calculation. Please add more code.', STATUS_ERROR);
 }
 
-if ($TPC < $percentage && $onlyEchoPercentage) {
-    echo sprintf('%0.2f', $TPC) . PHP_EOL;
-    exit(1);
+$totalPercentageCoverage = $coveredMetrics / $totalMetrics * 100;
+
+if ($totalPercentageCoverage < $percentage && ! $onlyEchoPercentage) {
+    printStatus(
+        'Total code coverage is '.formatCoverage($totalPercentageCoverage).' which is below the accepted '.$percentage.'%',
+        STATUS_ERROR
+    );
+}
+
+if ($totalPercentageCoverage < $percentage && $onlyEchoPercentage) {
+    printStatus(formatCoverage($totalPercentageCoverage), STATUS_ERROR);
 }
 
 if ($onlyEchoPercentage) {
-    echo sprintf('%0.2f', $TPC) . PHP_EOL;
-    exit(0);
+    printStatus(formatCoverage($totalPercentageCoverage));
 }
 
-echo 'Total code coverage is ' . sprintf('%0.2f', $TPC) . '% - OK!' . PHP_EOL;
+printStatus('Total code coverage is '.formatCoverage($totalPercentageCoverage).' – OK!');
